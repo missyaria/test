b838143bf63368974dce30ee43e46cb913ad890f
mw <mwd77@proton.me>
2023-12-01 11:30:55 +0100

    Added files related to the ls program

---
+++ ls.py
@@ -0,0 +1,4 @@
+import os
+path = os.getcwd()
+print(f"Vous vous trouvez actuellement dans le répertoire suivant : {os.getcwd()}")
+print(f"Ce répertoire contient les fichiers suivants : {os.listdir(path)}")
