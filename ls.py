5a39fda2d2b27a9f9cf555044137eda88c1f285e
mw <mwd77@proton.me>
2023-12-01 11:44:22 +0100

    Added an executable file and updated ls to ensure the window closes upon pressing a key

--- ls.py
+++ ls.py
@@ -2,3 +2,4 @@ import os
 path = os.getcwd()
 print(f"Vous vous trouvez actuellement dans le répertoire suivant : {os.getcwd()}")
 print(f"Ce répertoire contient les fichiers suivants : {os.listdir(path)}")
+close=input("Appuyez sur entrée pour quitter")
