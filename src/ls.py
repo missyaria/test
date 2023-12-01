6142e31bd2e7cd6042c2a541ee8b33edff592e57
mw <mwd77@proton.me>
2023-12-01 12:47:08 +0100

    Moved LISTING.py in the src folder

--- src/ls.py
+++
@@ -1,5 +0,0 @@
-import os
-path = os.getcwd()
-print(f"Vous vous trouvez actuellement dans le répertoire suivant : {os.getcwd()}")
-print(f"Ce répertoire contient les fichiers suivants : {os.listdir(path)}")
-close=input("Appuyez sur entrée pour quitter")
